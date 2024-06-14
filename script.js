17a12e7e168b8a4f9576972ac484d3dc81b57b56
Amir Ortega <amirortega21@gmail.com>
2024-06-14 19:46:36 -0300

    feat: add scrollReveal

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ d.addEventListener('click', e => {
     $navMenu.classList.toggle('show');
   }
 })
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', {delay: 500});
+ScrollReveal().reveal('.cards-banner-one', {delay: 500});
+ScrollReveal().reveal('.cards-banner-two', {delay: 500});
